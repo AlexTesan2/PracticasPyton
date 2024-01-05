6a920d3e2192ef37bac880aa09757e5dd9a24faf
Alejandro Tesán <tesan.deale22@zaragoza.salesianos.edu>
2024-01-05 19:11:08 +0100

    un pequeño codigo de prueba ...

---
+++ 0-TeLoPasoAmayus.py
@@ -0,0 +1,11 @@
+frase= 'colarse'
+print(str(frase).upper())
+
+
+
+def capitalize_first_letters(input_string):
+    return input_string.title()
+print(capitalize_first_letters("hello world"))  # Outputs: Hello World
+
+titulo='convert me tu capital letters'
+print(titulo.title())
